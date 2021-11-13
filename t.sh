bc04facb6abc4a8e38e1d0364d583253e56fa57f
iXploitID <93501879+iXploitID@users.noreply.github.com>
2021-11-13 19:36:29 +0700

    Add files via upload

---
+++ t.sh
@@ -0,0 +1,10 @@
+
+figlet SAYA|lolcat
+figlet HANYA|lolcat
+figlet MEMBANTU|lolcat
+figlet MEMPERMUDAH|lolcat
+figlet PEMAKAIAN|lolcat
+figlet Sc Ini |lolcat
+cp -f meta.sh $HOME
+cd $HOME
+bash meta.sh
